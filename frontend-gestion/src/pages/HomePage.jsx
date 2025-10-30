3e76c74ecaa6e95cb86f9932b040b7bfaa767b01
Arn0lf <perrotricefalo@gmail.com>
2025-10-30 15:52:14 -0400

    frontend con registro de usuario

---
+++ frontend-gestion/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Bienvenido a tu traductor de preferencias!</h1>
+      {/* The main dashboard will go here */}
+    </div>
+  );
+};
+
+export default HomePage;
