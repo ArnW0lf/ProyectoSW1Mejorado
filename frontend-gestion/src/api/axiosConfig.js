d66997323a5d42ac8403200f3a430790f7ac8205
Arnulfo Velasquez Päz <131307311+ArnW0lf@users.noreply.github.com>
2026-08-09 18:55:44 -0400

    Corregida la URL hardcodeada en axiosConfig

--- frontend-gestion/src/api/axiosConfig.js
+++ frontend-gestion/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api', // La URL de tu backend Django
+  baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : 'http://127.0.0.1:8000/api', // Usa la variable de entorno en Render o localhost por defecto
   headers: {
     'Content-Type': 'application/json',
   },
